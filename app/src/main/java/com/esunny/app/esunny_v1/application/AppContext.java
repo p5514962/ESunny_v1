b947b1a6f2c24a7c18a6ee50532093942c3028fc
p5514962 <20497342@qq.com>
2016-01-20 17:11:19 +0800

    update

---
+++ app/src/main/java/com/esunny/app/esunny_v1/application/AppContext.java
@@ -0,0 +1,14 @@
+package com.esunny.app.esunny_v1.application;
+
+import android.app.Application;
+
+/**
+ * @author KevinHo
+ * @version V1.0
+ * @ClassName:
+ * @Description:
+ * @email 20497342@qq.com
+ * @date
+ */
+public class AppContext extends Application {
+}
